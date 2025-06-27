de23d9ce8364bd9105b7aa2def3fd3984588136b
SumanBhandari69 <sumanbhandari6969@gmail.com>
2025-06-27 14:29:15 +0545

    markdown and postgres

--- djangocourse/settings.py
+++ djangocourse/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +43,7 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 THIRD_PARTY_APP = [
@@ -107,12 +109,15 @@ WSGI_APPLICATION = 'djangocourse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@db:5432/djangocourse',
+        conn_max_age=600
+    )
 }
+
+# postgresql://User:password@host:port/name
 
 #Customize User Models
 AUTH_USER_MODEL = "learn.UserProfile"
